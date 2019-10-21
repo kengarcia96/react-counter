8a5dcc216c52dfda03df1f3c0179a9206f499428
JOHN KENNETH GARCIA (PLAN-IRIS-ISD-OOCLL/MNL) <GARCIJO2@OOCL.COM>
2019-10-21 13:25:15 +0800

    Answered activity

--- src/App.js
+++ src/App.js
@@ -2,25 +2,35 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+class  App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {number:0}
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            React Counter Activity
+          </p>
+  
+          <div>          
+            <button onClick={() => {this.setState(state => ({number:state.number+1}))}}>+</button>
+            <button onClick={() => {this.setState(state => ({number:state.number-1}))}}>-</button>
+          </div>
+  
+           <div>{this.state.number}</div>
+  
+        </header>
+      </div>
+    );
+  }
+  }
+
 
 export default App;
